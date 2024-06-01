f876332531443dabb9e5c6bca87da86514a0ec78
Admin <Admin@DESKTOP-4K4NBNI>
2024-06-01 08:58:02 +0530

    1144 Harsh

---
+++ src/test/java/Exam/Gmail.java
@@ -0,0 +1,19 @@
+package Exam;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Gmail {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
